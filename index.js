66e47650275fe1b2fe27d56a2be39f5ff97a03ba
MJigah <jigah4july@gmailcom>
2022-10-29 15:14:31 +0100

    add changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/favicon.ico", function (req, res) {
 });
 
 app.get("/", (req, res) => {
-  res.setHeader("content-type", "text/plain");
+  // res.setHeader("content-type", "text/plain");
   res.send(JSON.stringify(getData));
 });
 
